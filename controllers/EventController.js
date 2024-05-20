e65b5340364614779752793c73a74a3b5932a8e7
Volodymyr Basok <volodymyrquo@gmail.com>
2024-05-20 21:43:56 +0300

    Add pagination

--- controllers/EventController.js
+++ controllers/EventController.js
@@ -1,55 +1,55 @@
-import EventModel from '../models/Event.js'
+import EventModel from '../models/Event.js';
 
-export const create = async (req,res)=>{
+export const create = async (req, res) => {
     try {
-
         const doc = new EventModel({
-            title:req.body.title,
-            description:req.body.description,
-            eventDate:req.body.eventDate,
-            organizer:req.body.organizer,
-            participant:req.body.participant
+            title: req.body.title,
+            description: req.body.description,
+            eventDate: req.body.eventDate,
+            organizer: req.body.organizer,
+            participant: req.body.participant,
         });
 
         const post = await doc.save();
 
-        res.json(post)
+        res.json(post);
     } catch (error) {
-        console.log(error)
-        res.status(500).json({massage:"Cannot create event"})
-   
+        console.log(error);
+        res.status(500).json({ massage: 'Cannot create event' });
     }
-}
+};
 
-
-export const getAll = async (req,res)=>{
+export const getAll = async (req, res) => {
     try {
-        const posts = await EventModel.find().populate('participant').exec();
-        res.json(posts)
+        const page = parseInt(req.query.page);
+        const pageSize = parseInt(req.query.pageSize);
+        const startIndex = (page - 1) * pageSize;
+        const endIndex = page * pageSize;
+        const events = await EventModel.find().populate('participant').exec();
+        const paginatedEvents = events.slice(startIndex, endIndex);
+        const totalPages = Math.ceil(events.length / pageSize);
+        res.json({ events: paginatedEvents, totalPages });
     } catch (error) {
-        console.log(error)
-        res.status(500).json({massage:"Cannot get events"})
-    
+        console.log(error);
+        res.status(500).json({ massage: 'Cannot get events' });
     }
-}
-export const getOne = async (req,res)=>{
+};
+export const getOne = async (req, res) => {
     try {
-        const eventId = req.params.id
+        const eventId = req.params.id;
         const doc = await EventModel.findOne({
-        _id:eventId,
-    }).populate('participant').exec()
-        if(!doc){
+            _id: eventId,
+        })
+            .populate('participant')
+            .exec();
+        if (!doc) {
             return res.status(404).json({
-                message:"Event not found"
-            })
+                message: 'Event not found',
+            });
         }
         res.json(doc);
-    
-    
     } catch (error) {
-        console.log(error)
-        res.status(500).json({massage:"Cannot get events"})
-    
+        console.log(error);
+        res.status(500).json({ massage: 'Cannot get events' });
     }
-    }
-
+};
